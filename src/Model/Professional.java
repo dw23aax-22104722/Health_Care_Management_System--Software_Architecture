fc367a6283bef4f69141b2dc654fcc50e7de6791
dw23aax-22104722 <dw23aax@herts.ac.uk>
2025-12-17 23:14:31 +0000

    Added Professional class which will be a base class. Removed view patient detail function

---
+++ src/Model/Professional.java
@@ -0,0 +1,51 @@
+package Model;
+
+public class Professional
+{
+    private String id;
+    private String name;
+    private String specialization;
+
+
+    public Professional(String id, String name, String specialization) {
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Professional{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", specialization='" + specialization + '\'' +
+                '}';
+    }
+}
