b7b5584336a8686f6a67c34fc0cd21284a11489a
dw23aax-22104722 <dw23aax@herts.ac.uk>
2025-12-22 00:14:29 +0000

    Updated patient class. Added toCSV and fromCSV method

--- src/Model/Patient.java
+++ src/Model/Patient.java
@@ -3,78 +3,26 @@ package Model;
 import java.util.*;
 
 public class Patient {
+    private String id;
+    private String name;
+    private String dob;
 
-
-    private String patientId;
-    private String patientName;
-    private String patientEmail;
-    private int patientAge;
-    private List<String> conditions;
-
-
-    public Patient(String patientId, String patientName, String patientEmail, int patientAge) {
-        this.patientId = patientId;
-        this.patientName = patientName;
-        this.patientEmail = patientEmail;
-        this.patientAge = patientAge;
-        this.conditions = new ArrayList<>();
+    public Patient(String id, String name, String dob) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
     }
 
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getDob() { return dob; }
 
-    public String getPatientId() {
-        return patientId;
+    public String toCSV() {
+        return id + "," + name + "," + dob;
     }
 
-    public String getPatientName() {
-        return patientName;
-    }
-
-    public String getPatientEmail() {
-        return patientEmail;
-    }
-
-    public int getPatientAge() {
-        return patientAge;
-    }
-
-    public List<String> getConditions() {
-        return conditions;
-    }
-
-
-    public void setPatientName(String patientName)
-    {
-        this.patientName = patientName;
-    }
-
-    public void setPatientEmail(String patientEmail) {
-        this.patientEmail = patientEmail;
-    }
-
-    public void setPatientAge(int patientAge) {
-        this.patientAge = patientAge;
-    }
-
-
-    public void addCondition(String condition) {
-        if (condition != null && !condition.isEmpty()) {
-            conditions.add(condition);
-        }
-    }
-
-    public void removeCondition(String condition) {
-        conditions.remove(condition);
-    }
-
-
-    @Override
-    public String toString() {
-        return "Patient{" +
-                "patientId='" + patientId + '\'' +
-                ", patientName='" + patientName + '\'' +
-                ", patientEmail='" + patientEmail + '\'' +
-                ", patientAge=" + patientAge +
-                ", conditions=" + conditions +
-                '}';
+    public static Patient fromCSV(String line) {
+        String[] p = line.split(",");
+        return new Patient(p[0], p[1], p[2]);
     }
 }
