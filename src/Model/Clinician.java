b5f6d08454443dbd959ff1c2661be77ac4be2edf
dw23aax-22104722 <dw23aax@herts.ac.uk>
2025-12-22 00:33:49 +0000

    Updated clinician class. Removed multiple attributes.

--- src/Model/Clinician.java
+++ src/Model/Clinician.java
@@ -1,103 +1,22 @@
 package Model;
 
-public class Clinician
-{
-    private String clinicianId;
-    private String firstName;
-    private String lastName;
-    private String title;
-    private String speciality;
+public class Clinician {
+    private String id;
+    private String name;
+    private String role;
 
-    private String phoneNumber;
-    private String email;
-
-    private String workplaceId;
-    private String workplaceType;
-
-    private String employmentStatus;
-    private String startDate;
-
-    public Clinician(
-            String clinicianId,
-            String firstName,
-            String lastName,
-            String title,
-            String speciality,
-            String phoneNumber,
-            String email,
-            String workplaceId,
-            String workplaceType,
-            String employmentStatus,
-            String startDate
-    ) {
-        this.clinicianId = clinicianId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.title = title;
-        this.speciality = speciality;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.workplaceId = workplaceId;
-        this.workplaceType = workplaceType;
-        this.employmentStatus = employmentStatus;
-        this.startDate = startDate;
+    public Clinician(String id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
     }
 
-    public String getClinicianId() {
-        return clinicianId;
+    public String toCSV() {
+        return id + "," + name + "," + role;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFullName() {
-        return firstName + " " + lastName;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getSpeciality() {
-        return speciality;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getWorkplaceId() {
-        return workplaceId;
-    }
-
-    public String getWorkplaceType() {
-        return workplaceType;
-    }
-
-    public String getEmploymentStatus() {
-        return employmentStatus;
-    }
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Clinician{" +
-                "id='" + clinicianId + '\'' +
-                ", name='" + getFullName() + '\'' +
-                ", speciality='" + speciality + '\'' +
-                ", workplace='" + workplaceId + '\'' +
-                '}';
+    public static Clinician fromCSV(String line) {
+        String[] c = line.split(",");
+        return new Clinician(c[0], c[1], c[2]);
     }
 }
